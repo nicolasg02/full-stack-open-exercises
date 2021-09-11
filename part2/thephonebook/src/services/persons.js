8111f380ce3b5ac424405ae7d34c59fdee3d601d
nicolasg02 <68668870+nicolasg02@users.noreply.github.com>
2021-09-11 03:15:27 -0300

    updated baseurl

--- part2/thephonebook/src/services/persons.js
+++ part2/thephonebook/src/services/persons.js
@@ -1,24 +1,23 @@
 import axios from 'axios';
-
-const baseUrl = 'http://localhost:3001/persons';
+const baseUrl = 'http://localhost:3001/api/persons';
 
 const getAll = () => {
   const request = axios.get(baseUrl);
-  return request.then(response => response.data);
+  return request.then((response) => response.data);
 };
 
-const create = contactObject => {
+const create = (contactObject) => {
   const request = axios.post(baseUrl, contactObject);
-  return request.then(response => response.data);
+  return request.then((response) => response.data);
 };
 
-const remove = contactId => {
+const remove = (contactId) => {
   return axios.delete(`${baseUrl}/${contactId}`);
 };
 
 const update = (id, newObject) => {
   const request = axios.put(`${baseUrl}/${id}`, newObject);
-  return request.then(response => response.data);
+  return request.then((response) => response.data);
 };
 
 export default {
